fe6fed53a56d4f1bd361f433188a17391f7da985
mnijaki <mnijaki@gmail.com>
2018-08-18 15:44:52 +0200

    Added more stuff

---
+++ Assets/Player/Scripts/MyNetworkBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+// Class overridning NetworkBehaviour.
+public class MyNetworkBehaviour: NetworkBehaviour
+{
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Private fields
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  // Input values.
+  private Vector3 input_val;
+
+  #endregion
+
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Private methods
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  // Event - called when local player is set up (overriden).
+  public override void OnStartLocalPlayer()
+  {
+    // Get camera component.
+    //GameObject camera = GetComponentInChildren(typeof(Camera),true).gameObject;
+    GameObject camera = gameObject.transform.GetChild(0).transform.gameObject;    
+    // Set camera as active.
+    camera.SetActive(true);
+    // Change color of local player.
+    Material mat = this.GetComponent<MeshRenderer>().material;
+    mat.color=Color.green;
+    this.GetComponent<MeshRenderer>().material=mat;
+
+
+    Debug.Log(Time.timeSinceLevelLoad+"OnStartLocalPlayer");
+  } // End of OnStartLocalPlayer
+
+  #endregion
+
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Private methods
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  // Update (called once per frame).
+  private void Update ()
+  {
+    // If not local player then exit from function.
+    if(!this.isLocalPlayer)
+    {
+      return;
+    }
+    // Get input values.
+    this.input_val.x=CrossPlatformInputManager.GetAxis("Horizontal");
+    this.input_val.y=0.0F;
+    this.input_val.z=CrossPlatformInputManager.GetAxis("Vertical");
+    // Actualize position.
+    this.transform.Translate(this.input_val);
+  } // End of Update
+
+  #endregion
+
+} //  End of MyNetworkBehaviour
---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// Own implementation of NetworkManager (overrides some of its methods and add some new ones).
+public class MyNetworkManager : NetworkManager
+{
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Public fields
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+
+
+  #endregion
+
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Private fields
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  #endregion
+
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Public methods
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  // Start hosting.
+  public void MyHostStart()
+  {
+    Debug.Log(Time.timeSinceLevelLoad+"MyHostStart");
+
+    // Create server and first client(who is hosting).
+    this.StartHost();
+  } // End of MyHostStart
+
+  // Event - on hosting has started (overriden).
+  public override void OnStartHost()
+  {
+    Debug.Log(Time.timeSinceLevelLoad+"OnStartHost");
+  } // End of OnStartHost
+
+  // Event - on client has started (overriden).
+  public override void OnStartClient(NetworkClient client)
+  {
+    Debug.Log(Time.timeSinceLevelLoad+"OnStartClient");
+  } // End of OnStartClient
+
+  // Event - called on client side, when he will connect to the server (overriden).
+  public override void OnClientConnect(NetworkConnection conn)
+  {
+    Debug.Log(Time.timeSinceLevelLoad+"OnClientConnect, IP="+conn.address);
+  } // End of OnClientConnect
+
+  #endregion
+
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Private methods
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  // Initialization.
+  private void Start()
+  {
+	} // End of Start
+
+  
+
+  #endregion
+
+
+
+
+
+
+
+
+
+
+
+} // End of MyNetworkManager
